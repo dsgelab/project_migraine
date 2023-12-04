3d05bc9127f78def67db06e85ffbab61236fcfd6
ettoreb17 <150792312+ettoreb17@users.noreply.github.com>
2023-12-04 14:19:32 +0200

    Update pipeline.R

--- finngen script/scripts/pipeline.R
+++ finngen script/scripts/pipeline.R
@@ -17,7 +17,7 @@ echo "Execution time: $execution_time seconds"
 
 echo "start NSAID users filtering"
 time0=$(date +%s)
-zcat /finngen/library-red/finngen_R12/phenotype_1.0/data/finngen_R12_detailed_longitudinal_1.0.txt.gz  | awk -F '\t' 'NR == 1 || (($2 == "PURCH") && (index($5, "M01A") != 0)) {print $0}' > /home/ivm/project_migraine/data/triptan_users.csv
+zcat /finngen/library-red/finngen_R12/phenotype_1.0/data/finngen_R12_detailed_longitudinal_1.0.txt.gz  | awk -F '\t' 'NR == 1 || (($2 == "PURCH") && (index($5, "M01A") != 0)) {print $0}' > /home/ivm/project_migraine/data/nsaid_users_file.csv
 time1=$(date +%s)
 execution_time=$((time1 - time0))
 echo "Execution time: $execution_time seconds"
