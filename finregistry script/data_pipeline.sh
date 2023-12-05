91f97a3a846b45b24ceaff4c08fa2141e6b66063
ferroteo <ferro.teo98@gmail.com>
2023-12-05 13:22:54 +0200

    fixed typo

--- finregistry script/data_pipeline.sh
+++ finregistry script/data_pipeline.sh
@@ -1,31 +1,32 @@
 
 # define file paths
 
-detailed_longitudinal = /data/processed_data/detailed_longitudinal/R10/detailed_longitudinal_DF10_2022-11-11.csv
-endpointer = /data/processed_data/endpointer/longitudinal_endpoints_no_omits_DF10_2022_09_29.csv
+detailed_longitudinal=/data/processed_data/detailed_longitudinal/R10/detailed_longitudinal_DF10_2022-11-11.csv
+endpointer=/data/processed_data/endpointer/longitudinal_endpoints_no_omits_DF10_2022_09_29.csv
 
-cluster_migraine_events_file = /data/projects/project_mferro/project_migraine/data/endpoint_migraine.csv
-triptan_users_file = /data/projects/project_mferro/project_migraine/data/triptan_users.csv
-nsaid_users_file = /data/projects/project_mferro/project_migraine/data/nsaid_users.csv
+cluster_migraine_events_file=/data/projects/project_mferro/project_migraine/data/endpoint_migraine.csv
+triptan_users_file=/data/projects/project_mferro/project_migraine/data/triptan_users.csv
+nsaid_users_file=/data/projects/project_mferro/project_migraine/data/nsaid_users.csv
 
 # execute this script in order to generate the study population 
 
 echo "start triptane users filtering"
 time0=$(date +%s)
-awk -F ',' 'NR == 1 || (($6 == "PURCH") && (index($9, "N02CC") != 0)) {print $0}' ${detailed_longitudinal} > ${triptan_users_file}
+awk -F ',' 'NR == 1 || (($6 == "PURCH") && (index($9, "N02CC") != 0)) {print $0}' $detailed_longitudinal > $triptan_users_file
 time1=$(date +%s)
 execution_time=$((time1 - time0))
 echo "Execution time: $execution_time seconds"
 
 echo "start cluster headache endpoint filtering"
 time0=$(date +%s)
-awk -F '\t' 'NR == 1 || ($6 == "G6_CLUSTHEADACHE_WIDE") {print $0}' ${endpointer}> ${cluster_migraine_events_file}time1=$(date +%s)
+awk -F '\t' 'NR == 1 || ($6 == "G6_CLUSTHEADACHE_WIDE") {print $0}' $endpointer > $cluster_migraine_events_file
+time1=$(date +%s)
 execution_time=$((time1 - time0))
 echo "Execution time: $execution_time seconds"
 
 echo "start NSAID users filtering"
 time0=$(date +%s)
-awk -F '\t' 'NR == 1 || (($2 == "PURCH") && (index($5, "M01A") != 0)) {print $0}' ${detailed_longitudinal} > ${nsaid_users_file}
+awk -F '\t' 'NR == 1 || (($2 == "PURCH") && (index($5, "M01A") != 0)) {print $0}' $detailed_longitudinal > $nsaid_users_file
 time1=$(date +%s)
 execution_time=$((time1 - time0))
 echo "Execution time: $execution_time seconds"
