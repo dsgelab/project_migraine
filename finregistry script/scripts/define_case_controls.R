17a3fea1f1c6851b0a1eea0f8facad6118e52f8c
ferroteo <ferro.teo98@gmail.com>
2023-12-07 10:59:19 +0200

    fixed output display

--- finregistry script/scripts/define_case_controls.R
+++ finregistry script/scripts/define_case_controls.R
@@ -42,9 +42,9 @@ cohort <- cohort %>%
   mutate(SWITCH = ifelse(ATC_CODE != lag(ATC_CODE),1,0)) %>%
   mutate(SWITCH = ifelse(is.na(SWITCH),0,SWITCH)) %>%
   mutate(VISIT_NUMBER = row_number()) %>%  
-  mutate(F1 = ifelse(row_number() == 1 & ATC_CODE != lead(ATC_CODE) & lead(ATC_CODE) == lead(ATC_CODE,2) & lead(ATC_CODE,2) == lead(ATC_CODE, 3), 1, 0))  %>% 
+  mutate(F1 = ifelse(row_number() == 2 & ATC_CODE != lag(ATC_CODE) & ATC_CODE == lead(ATC_CODE) & ATC_CODE == lead(ATC_CODE, 2), 1, 0))  %>% 
   mutate(F2 = ifelse(row_number() == 1 & ATC_CODE != lead(ATC_CODE)& lead(ATC_CODE) != lead(ATC_CODE, 2) & lead(ATC_CODE, 2) == lead(ATC_CODE, 3) & lead(ATC_CODE, 3) == lead(ATC_CODE, 4), 1, 0)) %>%
-  mutate(F3 = ifelse((row_number() == 1 & ATC_CODE != lead(ATC_CODE) & lead(ATC_CODE) != lead(ATC_CODE, 2) & lead(ATC_CODE, 2) != lead(ATC_CODE, 3)), 1, 0)) %>%  
+  mutate(F3 = ifelse ((row_number() == 1 & ATC_CODE != lead(ATC_CODE) & lead(ATC_CODE) != lead(ATC_CODE, 2) & lead(ATC_CODE, 2) != lead(ATC_CODE, 3)), 1, 0)) %>%  
   mutate(control_e = ifelse((row_number() == 1 & ATC_CODE == lead(ATC_CODE) & ATC_CODE == lead(ATC_CODE,2) & ATC_CODE == lead(ATC_CODE,3)), 1, 0))
 
 # QC:
@@ -60,35 +60,35 @@ F2 <- cohort %>% filter(F2==1) %>% pull(FINREGISTRYID)
 F3 <- cohort %>% filter(F3==1) %>% pull(FINREGISTRYID)
 
 cat( ' ----- naive definition ----- \n',
-  'number of failures to 1 triptan: ', length(TRPT_FAIL_1), 
-  'in percentage: ',100*length(TRPT_FAIL_1)/ORIGINAL_N,'\n',
-  'number of failures to 2 triptan: ', length(TRPT_FAIL_2), 
-  'in percentage: ',100*length(TRPT_FAIL_2)/ORIGINAL_N,'\n',
-  'number of failures to >= 3 triptan: ', length(TRPT_FAIL_3), 
-  'in percentage: ',100*length(TRPT_FAIL_3)/ORIGINAL_N,'\n \n',
-  ' ----- Based on first 5 purchases ----- \n',
-  'number of failures to 1 triptan: ', length(F1), 
-  'in percentage: ',100*length(F1)/ORIGINAL_N,'\n',
-  'number of failures to 2 triptan: ', length(F2), 
-  'in percentage: ',100*length(F2)/ORIGINAL_N,'\n',
-  'number of failures to >= 3 triptan: ', length(F3), 
-  'in percentage: ',100*length(F3)/ORIGINAL_N,'\n'
+     'number of failures to 1 triptan: ', length(TRPT_FAIL_1), 
+     'in percentage: ',100*length(TRPT_FAIL_1)/ORIGINAL_N,'\n',
+     'number of failures to 2 triptan: ', length(TRPT_FAIL_2), 
+     'in percentage: ',100*length(TRPT_FAIL_2)/ORIGINAL_N,'\n',
+     'number of failures to >= 3 triptan: ', length(TRPT_FAIL_3), 
+     'in percentage: ',100*length(TRPT_FAIL_3)/ORIGINAL_N,'\n \n',
+     ' ----- Based on first 5 purchases ----- \n',
+     'number of failures to 1 triptan: ', length(F1), 
+     'in percentage: ',100*length(F1)/ORIGINAL_N,'\n',
+     'number of failures to 2 triptan: ', length(F2), 
+     'in percentage: ',100*length(F2)/ORIGINAL_N,'\n',
+     'number of failures to >= 3 triptan: ', length(F3), 
+     'in percentage: ',100*length(F3)/ORIGINAL_N,'\n','\n'
 )
 
-  
+
 ###############
 ####   3   ####
 ###############
-  
+
 CONTROL <- cohort %>% summarise(TOT_SWITCH=sum(SWITCH)) %>% filter(TOT_SWITCH==0) %>% pull(FINREGISTRYID)
 CTRL <- cohort %>% filter(control_e==1) %>% pull(FINREGISTRYID)
 
 cat(' ----- naive definition ----- \n',
-  'number of failures to 0 triptans (controls): ', length(CONTROL), 
-  'in percentage: ',100*length(CONTROL)/ORIGINAL_N,'\n',
-  ' ----- Based on first 5 purchases ----- \n',
-  '>=3 triptans purchases without switching controls): ', length(CTRL), 
-  'in percentage: ',100*length(CTRL)/ORIGINAL_N
+    'number of failures to 0 triptans (controls): ', length(CONTROL), 
+    'in percentage: ',100*length(CONTROL)/ORIGINAL_N,'\n','\n',
+    ' ----- Based on first 5 purchases ----- \n',
+    '>=3 triptans purchases without switching controls): ', length(CTRL), 
+    'in percentage: ',100*length(CTRL)/ORIGINAL_N
 )  
 
 
@@ -104,9 +104,7 @@ if( nrow(cohort) != SC ){
 # overwrite the file to add the case-control results
 fwrite(cohort, file=study_population_file, append = FALSE)
 
-# clean up memory
-rm(list=ls())
-gc()
+
 
 
 
