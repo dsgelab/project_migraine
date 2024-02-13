364d9c6b3728d337db39fac3868b23dc69389db3
ferroteo <ferro.teo98@gmail.com>
2024-02-13 09:26:10 +0200

    update: generates a different file depending on the required timewindow

--- finregistry script/scripts/make_SampleList.R
+++ finregistry script/scripts/make_SampleList.R
@@ -13,14 +13,15 @@ source('/data/projects/project_mferro/project_migraine/file_paths.R')
 cohort <- fread(study_population_file)
 
 # Make Sample File
-WASHOUT_TIME = 30 
+WASHOUT_TIME = 30 # days
+TIME_WINDOW = 10 # years
 
 sample_file <- cohort %>% 
     arrange(FINREGISTRYID, EVENT_DAY) %>% 
     group_by(FINREGISTRYID) %>% 
     mutate(date_of_birth= as.Date(BIRTH_DATE),
         # start 10 years before first purchase (+ washout)
-        start_of_followup=as.Date(DATE_FIRST_PURCH)- WASHOUT_TIME - (365*10),
+        start_of_followup=as.Date(DATE_FIRST_PURCH)- WASHOUT_TIME - (365*TIME_WINDOW),
         # end before first triptan purchase
         # NB: triptan avarage pack size is 28 
         end_of_followup=as.Date(DATE_FIRST_PURCH)- WASHOUT_TIME
@@ -29,7 +30,8 @@ sample_file <- cohort %>%
     select(FINREGISTRYID,date_of_birth, start_of_followup, end_of_followup) %>%
     distinct(FINREGISTRYID, .keep_all = TRUE)
 
-fwrite(sample_file, file=SampleList, append = FALSE)
+outfile = paste0(SampleList,'_',as.character(TIME_WINDOW),'_years')
+fwrite(sample_file, file=outfile, append = FALSE)
 
 
 
